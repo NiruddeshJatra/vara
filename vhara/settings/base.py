345a1e9bf9161929d48436b36bd4eb7b58e34819
Nasiful Alam <nasifulalam1212@gmail.com>
2025-01-04 23:45:41 +0600

    created necessary apps

--- vhara/settings/base.py
+++ vhara/settings/base.py
@@ -28,6 +28,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'advertisements',
+    'complaints',
+    'rentals',
+    'reviews',
+    'users',
+    'payments',
 ]
 
 MIDDLEWARE = [
