adb8f88e745513c543d42304d6f8067c27eedbab
Nasiful Alam <nasifulalam1212@gmail.com>
2025-04-29 01:34:15 +0600

    Fix product setting for media

--- backend/bhara/settings/production.py
+++ backend/bhara/settings/production.py
@@ -66,14 +66,15 @@ USE_S3 = all([
 ])
 
 if USE_S3:
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    # Use custom MediaStorage for media files (user uploads)
+    DEFAULT_FILE_STORAGE = 'bhara.storage_backends.MediaStorage'
+    # Use S3Boto3Storage for static files only
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = os.environ.get('AWS_REGION', 'us-east-1')
-    AWS_S3_CUSTOM_DOMAIN = os.environ.get('CLOUDFRONT_DOMAIN')
-    AWS_LOCATION = 'static'
+    AWS_S3_CUSTOM_DOMAIN = os.environ.get('CLOUDFRONT_DOMAIN') or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 else:
